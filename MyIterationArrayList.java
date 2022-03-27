b420540faabb1489d4d3a760cd4288ae3aee7d20
mehdiyabanod <102464103+mehdiyabanod@users.noreply.github.com>
2022-03-27 15:46:58 +0530

    Add files via upload

---
+++ MyIterationArrayList.java
@@ -0,0 +1,39 @@
+package p1;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class MyIterationArrayList {
+
+	public static void main(String[] args) {
+		int e;
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Integer> I = new ArrayList<Integer>();
+		System.out.println("How many elements you want");
+		int n = sc.nextInt();
+		System.out.println("Enter" + n + "elements into arraylist");
+		for (int i = 1; i <= n; i++) {
+			e = sc.nextInt();
+			I.add(e);
+		}
+		System.out.println("Using list object" + I);
+
+		System.out.println("using for loop");
+
+		for (int i = 0; i < I.size(); i++) {
+			System.out.println(I.get(i));
+		}
+		System.out.println("using enhanced for loop");
+		for (Integer i : I) {
+			System.out.println(i);
+		}
+		System.out.println("Iterating elements using Iterator interface");
+		Iterator it = I.iterator();
+		while (it.hasNext()) {
+			System.out.println(it.next());
+
+		}
+	}
+
+}
