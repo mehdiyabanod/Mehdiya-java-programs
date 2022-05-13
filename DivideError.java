8bb662f5a62f83b9329cbe38df4867a36c4776e9
mehdiya bano D <102464103+mehdiyabanod@users.noreply.github.com>
2022-05-13 21:55:43 +0530

    Add files via upload

---
+++ DivideError.java
@@ -0,0 +1,18 @@
+package p1;
+
+
+
+public class DivideError {
+
+	public static void main(String[] args) {
+	System.out.println("Main");
+		int a=12, b=0, c=0;
+		System.out.println("before division");
+		c=a/b;
+		System.out.println("After division");
+		System.out.println("c="+c);
+			
+	
+	}
+
+}
