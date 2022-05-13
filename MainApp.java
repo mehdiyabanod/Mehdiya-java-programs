7773c5540f0bf8939f0d2a98e7b18b9b10326291
mehdiya bano D <102464103+mehdiyabanod@users.noreply.github.com>
2022-05-13 22:30:21 +0530

    Add files via upload

--- MainApp.java
+++ MainApp.java
@@ -1,29 +1,50 @@
-package p1;
+package com.edu.student;
+
+import java.util.Scanner;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		// Annonymous class
-	
-		new Thread() {
-			public void run() {
-				System.out.println("Run method");
-			}
+		// Menu from user
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			System.out.println("Database Operations");
+			System.out.println("Enter your choice");
+			System.out.println("1. To display student information");
+			System.out.println("2. Insert student record");
+			System.out.println("3. update student information based on id");
+			System.out.println("4. delete student information based on id");
+			System.out.println("5. select student based on ID");
+			int ch = sc.nextInt();
+			switch(ch) {
+			case 1:
+				StudentDataOperations.displayStudentInfo();
+				break;
+			case 2:
+				StudentDataOperations.insertStudentInfo();
+				break;
 
-		}.start();
-		Runnable rob=new Runnable() {
-			public void run() {
-				System.out.println("Runnable run method");
-				
-			}
-		};
-		Thread tob1=new Thread(rob);
-		tob1.start();
-		
-	
+			case 3:
+				StudentDataOperations.updateStudentInfo();
+				break;
+			case 4:
+				StudentDataOperations.deleteStudentInfo();
+				break;
+			case 5:
+				StudentDataOperations.selectStudentInfo();
+				break;
+			default:System.out.println("Invalid choice");
 			
-		}
-		
-	}
+			}//switch
+			System.out.println("Do you want to continue yes/no");
+			String choice=sc.next();
+			if(choice.equalsIgnoreCase("no")) {
+				break;
+				
+			}//if
 
+		}//while end
+System.out.println("program terminated");
+	}//main
 
+}//main app
