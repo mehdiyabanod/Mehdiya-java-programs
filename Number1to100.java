425064ea840488baf0662d79ac80338c42d6e585
mehdiya bano D <102464103+mehdiyabanod@users.noreply.github.com>
2022-05-13 22:18:58 +0530

    Add files via upload

---
+++ Number1to100.java
@@ -0,0 +1,18 @@
+package p1;
+
+public class Number1to100 {
+
+	public static void main(String[] args) {
+		
+		int i,sum=0;
+		i=1;
+		while(i<=100)
+		{
+			sum=sum+i;
+			i=i+1;
+			
+		}
+System.out.println("sum"+sum);
+	}
+
+}
