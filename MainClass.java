71f3111662afdfe26f56f4158988b71e711a66c9
mehdiya bano D <102464103+mehdiyabanod@users.noreply.github.com>
2022-05-13 21:59:37 +0530

    Add files via upload

--- MainClass.java
+++ MainClass.java
@@ -1,32 +1,55 @@
-package p1;
+//polymorphism 2types: compile time within class function Overloading
+//run time inheritance function overloading
+package p2;
 
-import java.util.Scanner;
+class DisplayOverload {
+	void display() {// no argument function
+		System.out.println("No argument display");
+	}
 
-//user defined Exception
-class CheckAge extends Exception {
-	public CheckAge(String s) {
-		super(s);// call constructor
+void display(int i) {
+	System.out.println("int one argument function "+i);
+}
+
+	void display(float i) {
+		System.out.println("float one argument function " + i);
+	}
+
+	void display(int i, int j) {
+		System.out.println("two int argument function " + i + "and " + j);
+	}
+
+	void display(int i, int j, int k) {
+		System.out.println("three int argument function " + i + "," + j + "and" + k);
+	}
+
+	void display(float i, float j) {
+		System.out.println("two float argument function " + i + "and" + j);
+	}
+
+	void display(int i, float j) {// by changing order
+		System.out.println("int and float two argument function " + i + "and" + j);
+	}
+
+	void display(float j, int i) {
+		System.out.println("float and int two argument function " + i + "and" + j);
+	}
+
+	void display(byte p) {
+		System.out.println("byte int" + p);
 	}
 }
 
 public class MainClass {
 
 	public static void main(String[] args) {
-		int age;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter age");
-		age=sc.nextInt();
-		try {
-			if(age<18) {
-				throw new CheckAge("Not Eligible for voting");
-				}
-			else {
-				System.out.println("You can vote");
-			}
-		}catch(CheckAge e) {
-			System.out.println("object"+e);//object printing
-			e.printStackTrace();
-		}
+		DisplayOverload ob = new DisplayOverload();
+		ob.display();// no arg
+		ob.display((byte) 4);// one int
+		ob.display(4.5f);
+		ob.display(4, 6);
+		ob.display(2, 6, 8);
+
 	}
 
 }
