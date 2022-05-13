c797bcb80e50b770f409693433288ef63d8e5775
mehdiya bano D <102464103+mehdiyabanod@users.noreply.github.com>
2022-05-13 22:21:28 +0530

    Add files via upload

---
+++ MainClassRunnable.java
@@ -0,0 +1,23 @@
+package p1;
+Class MyClass implements Runnable{
+	@override
+	public void run() {
+		System.out.println("Inside run method"+Thread.currentThread());
+	
+	}
+
+}
+
+public class MainClassRunnable {
+
+	public static void main(String[] args) {
+		MyClass ob =new MyClass();
+		Thread tob =new Thread(ob);
+		tob.setName("t1");
+		tob.start();
+	
+
+	}
+
+}
+	
