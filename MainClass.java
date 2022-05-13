24fc15f90c96ff83785386df03d09a28e37dc2d3
mehdiya bano D <102464103+mehdiyabanod@users.noreply.github.com>
2022-05-13 22:18:35 +0530

    Add files via upload

--- MainClass.java
+++ MainClass.java
@@ -1,15 +1,35 @@
-package edu.medh;
-class MyThread extends Thread{
-	public void run() {
-		System.out.println("run method");
+package p1;
+
+import java.lang.reflect.Constructor;
+import java.util.Scanner;
+
+
+
+class SoftwareEmployee {
+	void input() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter name");
+		String name = sc.next();
+		System.out.println("enter age");
+		int age = sc.nextInt();
+
+	}
+
+	void display() {
+		String name ="";
+		System.out.println("name=" + name);
+		int age = 0;
+		System.out.println("age=" + age);
 	}
 }
 
 public class MainClass {
 
 	public static void main(String[] args) {
-		MyThread ob=new MyThread();
-		ob.start();
+		SoftwareEmployee ob = new SoftwareEmployee();
+		ob.input();
+		ob.display();
+
 	}
 
 }
