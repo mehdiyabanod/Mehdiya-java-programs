41524c30f62c7e4d938e2d280099f3e1b7d9d55c
mehdiya bano D <102464103+mehdiyabanod@users.noreply.github.com>
2022-05-13 22:24:58 +0530

    Add files via upload

---
+++ StringFunction.java
@@ -0,0 +1,18 @@
+package mehdiya.com;
+
+public class StringFunction {
+
+	public static void main(String[] args) {
+		String s="Edubridge India";
+		System.out.println(s.substring(6));
+		System.out.println(s.substring(4,10));
+		
+		System.out.println(s.indexOf('i'));
+		
+		
+		System.out.println(s.lastIndexOf('i'));
+		
+
+	}
+
+}
