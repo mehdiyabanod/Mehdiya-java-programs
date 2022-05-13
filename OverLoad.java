5c6eded054b961ce1ba162fec84c8289f8b3a24d
mehdiya bano D <102464103+mehdiyabanod@users.noreply.github.com>
2022-05-13 22:00:22 +0530

    Add files via upload

--- OverLoad.java
+++ OverLoad.java
@@ -1,25 +1,47 @@
-package p1;
+package p2;
 
 public class OverLoad {
-	void series() {
+//function with argument
 
+	void series(int x, int n) {
+		double s = 0;
+		for (int i = 1; i <= n; i++) {
+			s = s + Math.pow(x, i);
+		}
+		System.out.println("sum=" + s);
 	}
 
 	void series(int p) {
-		int cube = 0;
-		for (int i = 1; i <= 5; i++) {
-			cube = i * i * i;
-			System.out.println(cube+",");
+		for (int i = 1; i <= p; i++) {
+			int t = 0;
+			t = (i * i * i) - 1;
+
+			System.out.println(t + ",");
 		}
 	}
 
-	void series(int x, int n) {
-
+	void series() {
+		double s = 0;
+		for (int i = 2; i <= 10; i++) {
+			s = s + ((double) 1 / i);
+		}
+		System.out.println("sum=" + s);
 	}
 
 	public static void main(String[] args) {
-OverLoad ob=new OverLoad();
-ob.series(5);
+		OverLoad ob = new OverLoad();
+		ob.series(8);
+		ob.series();
+		ob.series(2, 5);
+		int i = 8;
+		short s;
+		s = (short) i;
+		// i=s implicit
+		float s1 = 56.7f;
+		double d;
+		d = s1;// implicit
+		s1 = (float) d;
+
 	}
 
 }
