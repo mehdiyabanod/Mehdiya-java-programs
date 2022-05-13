5d68e9f9d7f786bc9777cd801937b9e67f22eb3f
mehdiya bano D <102464103+mehdiyabanod@users.noreply.github.com>
2022-05-13 21:52:09 +0530

    Add files via upload

---
+++ MyClassFinalDemo.java
@@ -0,0 +1,19 @@
+package p2;
+
+class MyBank{
+	final int accno=8764;
+	void modify() {
+		System.out.println("Modify");
+		
+	}
+}
+public class MyClassFinalDemo extends MyBank {
+void modify() {
+	System.out.println("Modify");
+	
+}
+	public static void main(String[] args) {
+		
+	}
+
+}
