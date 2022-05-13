baf5dc764c46f0b3f0a26a3be50c9ab4fa97494b
mehdiya bano D <102464103+mehdiyabanod@users.noreply.github.com>
2022-05-13 22:23:00 +0530

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,21 +1,10 @@
-package p1;
+package mehdiya.com;
 
 public class HelloWorld {
-	int i;
-	float f;
-	char ch;
-	double d;
-	String s;
-	
 
 	public static void main(String[] args) {
-		HelloWorld ob=new HelloWorld();
-		System.out.println("Default values of instance variable");
-		System.out.println("int="+ob.i);
-		System.out.println("float="+ob.f);
-		System.out.println("double="+ob.d);
-		System.out.println("char="+ob.ch);
-		System.out.println("String="+ob.s);
+
+		System.out.println("Hello world");
 	}
 
 }
