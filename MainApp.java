0b36d8ea21e491cae128677e4e467c9195836192
mehdiya bano D <102464103+mehdiyabanod@users.noreply.github.com>
2022-05-13 22:25:55 +0530

    Add files via upload

--- MainApp.java
+++ MainApp.java
@@ -1,36 +1,29 @@
 package p1;
 
-class OuterClass {
-	int i;
-
-	private class InnerClass {
-		int varinner;
-
-		InnerClass() {
-			varinner = 10;
-
-		}
-
-		void method_inner() {
-			System.out.println("InnerClass Method=" + varinner);
-			System.out.println("outer class variable i=" + i);
-		}
-
-	}// Inner class end
-
-	void outer_method() {
-		InnerClass ob = new InnerClass();
-		ob.method_inner();// call inner class method
-
-	}
-
-}
-
 public class MainApp {
 
 	public static void main(String[] args) {
-OuterClass ob=new OuterClass();
-ob.outer_method();
+		// Annonymous class
+	
+		new Thread() {
+			public void run() {
+				System.out.println("Run method");
+			}
+
+		}.start();
+		Runnable rob=new Runnable() {
+			public void run() {
+				System.out.println("Runnable run method");
+				
+			}
+		};
+		Thread tob1=new Thread(rob);
+		tob1.start();
+		
+	
+			
+		}
+		
 	}
 
-}
+
